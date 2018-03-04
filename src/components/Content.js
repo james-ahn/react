0e40877ef4b4dacef6e9333de5d2d355ee37aa25
Jame Ahn <jamesahn.dev@gmail.com>
2018-03-04 12:20:19 -0400

    invoke how to use props

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,11 +5,17 @@ class Content extends React.Component {
     render(){
         return (
             <div>
-                <h2>Content</h2>
-                <p> Hey! </p>
+                <h2>{this.props.title}</h2>
+                <p>{this.props.body}</p>
             </div>
         );
     }
 }
+
+//Type validate
+Content.propTypes = {
+    title: React.PropTypes.string,
+    body: React.PropTypes.string.isRequired //isRequired is necessary
+};
 
 export default Content;
